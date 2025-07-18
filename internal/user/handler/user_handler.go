e9a070d7696d90646794f78df9f955bf7965ff8c
ramdhanimichan <ramdhani.michan48@gmail.com>
2025-07-19 00:28:44 +0700

    update : reponse handler user

--- internal/user/handler/user_handler.go
+++ internal/user/handler/user_handler.go
@@ -3,6 +3,8 @@ package handler
 import (
 	"digital-library-dashboard/internal/user/model"
 	"digital-library-dashboard/internal/user/service"
+	"digital-library-dashboard/pkg/utils"
+	"net/http"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -20,7 +22,7 @@ func RegisterRoutes(r fiber.Router, svc service.Service) {
 func (h *Handler) Register(c *fiber.Ctx) error {
 	var body model.RegisterRequest
 	if err := c.BodyParser(&body); err != nil {
-		return c.Status(400).JSON(fiber.Map{"error": "invalid request"})
+		return utils.ErrorResponse(c, http.StatusBadRequest, "Invalid request")
 	}
 	user := model.User{
 		Name:     body.Name,
@@ -29,19 +31,19 @@ func (h *Handler) Register(c *fiber.Ctx) error {
 		Role:     body.Role,
 	}
 	if err := h.svc.Register(&user); err != nil {
-		return c.Status(500).JSON(fiber.Map{"error": err.Error()})
+		return utils.ErrorResponse(c, http.StatusInternalServerError, err.Error())
 	}
-	return c.JSON(user)
+	return utils.SuccessResponse(c, http.StatusOK, "Success register", user)
 }
 
 func (h *Handler) Login(c *fiber.Ctx) error {
 	var cred model.Credentials
 	if err := c.BodyParser(&cred); err != nil {
-		return c.Status(400).JSON(fiber.Map{"error": "invalid request"})
+		return utils.ErrorResponse(c, http.StatusBadRequest, "Invalid request")
 	}
-	token, role, err := h.svc.Login(cred.Email, cred.Password)
+	token, _, err := h.svc.Login(cred.Email, cred.Password)
 	if err != nil {
-		return c.Status(401).JSON(fiber.Map{"error": "invalid credentials"})
+		return utils.ErrorResponse(c, http.StatusUnauthorized, "Invalid credentials")
 	}
-	return c.JSON(fiber.Map{"token": token, "role": role})
+	return utils.SuccessResponse(c, http.StatusOK, "Success login", fiber.Map{"token": token})
 }
