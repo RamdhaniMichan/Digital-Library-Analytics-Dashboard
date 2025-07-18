2023651d43151d3f99c3f8f29b925f46fb6300ea
ramdhanimichan <ramdhani.michan48@gmail.com>
2025-07-19 00:30:02 +0700

    add : middleware auth

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -2,36 +2,43 @@ package middleware
 
 import (
 	"digital-library-dashboard/internal/user/model"
-	"os"
+	"digital-library-dashboard/pkg/utils"
+	"strings"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/golang-jwt/jwt"
 )
 
 func JWTMiddleware() fiber.Handler {
 	return func(c *fiber.Ctx) error {
-		tokenStr := c.Get("Authorization")
-		if tokenStr == "" {
-			return c.Status(401).JSON(fiber.Map{"error": "Missing token"})
+		authHeader := c.Get("Authorization")
+		if authHeader == "" {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "missing token"})
 		}
-		token, err := jwt.Parse(tokenStr, func(t *jwt.Token) (interface{}, error) {
-			return []byte(os.Getenv("JWT_SECRET")), nil
+
+		tokenStr := strings.TrimPrefix(authHeader, "Bearer ")
+
+		claims, err := utils.ParseJWT(tokenStr)
+		if err != nil {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "invalid token"})
+		}
+
+		c.Locals("user", &model.UserClaim{
+			ID:   claims.UserID,
+			Role: claims.Role,
 		})
-		if err != nil || !token.Valid {
-			return c.Status(401).JSON(fiber.Map{"error": "Invalid token"})
-		}
+
 		return c.Next()
 	}
 }
 
 func RoleMiddleware(roles ...string) fiber.Handler {
 	return func(c *fiber.Ctx) error {
-		user := c.Locals("user").(*model.User)
+		user := c.Locals("user").(*model.UserClaim)
 		for _, r := range roles {
 			if user.Role == r {
 				return c.Next()
 			}
 		}
-		return c.Status(403).JSON(fiber.Map{"error": "forbidden"})
+		return c.Status(403).JSON(fiber.Map{"error": "forbidden access"})
 	}
 }
