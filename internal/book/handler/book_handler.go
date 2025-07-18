fcc0e9bbc0005ad98215e8235339d2947cbb19fb
ramdhanimichan <ramdhani.michan48@gmail.com>
2025-07-19 00:29:28 +0700

    update : reponse handler book

--- internal/book/handler/book_handler.go
+++ internal/book/handler/book_handler.go
@@ -3,6 +3,9 @@ package handler
 import (
 	"digital-library-dashboard/internal/book/model"
 	"digital-library-dashboard/internal/book/service"
+	"digital-library-dashboard/internal/middleware"
+	userModel "digital-library-dashboard/internal/user/model"
+	"digital-library-dashboard/pkg/utils"
 	"net/http"
 	"strconv"
 
@@ -10,7 +13,8 @@ import (
 )
 
 func RegisterRoutes(r fiber.Router, svc service.Service) {
-	h := handler{svc: svc}
+	h := Handler{svc: svc}
+	r.Use(middleware.RoleMiddleware("admin"))
 	r.Get("/books", h.GetAll)
 	r.Get("/books/:id", h.GetByID)
 	r.Post("/books", h.Create)
@@ -18,64 +22,67 @@ func RegisterRoutes(r fiber.Router, svc service.Service) {
 	r.Delete("/books/:id", h.Delete)
 }
 
-type handler struct {
+type Handler struct {
 	svc service.Service
 }
 
-func (h *handler) GetAll(c *fiber.Ctx) error {
+func (h *Handler) GetAll(c *fiber.Ctx) error {
 	books, err := h.svc.GetAll()
 	if err != nil {
-		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+		return utils.ErrorResponse(c, http.StatusInternalServerError, err.Error())
 	}
-	return c.JSON(books)
+	return utils.SuccessResponse(c, http.StatusOK, "Success get books", books)
 }
 
-func (h *handler) GetByID(c *fiber.Ctx) error {
+func (h *Handler) GetByID(c *fiber.Ctx) error {
 	id, err := strconv.Atoi(c.Params("id"))
 	if err != nil {
-		return c.Status(http.StatusBadRequest).JSON(fiber.Map{"error": "Invalid book ID"})
+		return utils.ErrorResponse(c, http.StatusBadRequest, "Invalid book ID")
 	}
 	book, err := h.svc.GetByID(id)
 	if err != nil {
-		return c.Status(http.StatusNotFound).JSON(fiber.Map{"error": "Book not found"})
+		return utils.ErrorResponse(c, http.StatusNotFound, "Book not found")
 	}
-	return c.JSON(book)
+	return utils.SuccessResponse(c, http.StatusOK, "Success get book", book)
 }
 
-func (h *handler) Create(c *fiber.Ctx) error {
+func (h *Handler) Create(c *fiber.Ctx) error {
 	var b model.Book
+	user := c.Locals("user").(*userModel.UserClaim)
 	if err := c.BodyParser(&b); err != nil {
-		return c.Status(http.StatusBadRequest).JSON(fiber.Map{"error": "Invalid input"})
+		return utils.ErrorResponse(c, http.StatusBadRequest, "invalid input")
 	}
+
+	b.CreatedBy = user.ID
 	if err := h.svc.Create(b); err != nil {
-		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+		return utils.ErrorResponse(c, http.StatusInternalServerError, err.Error())
 	}
-	return c.SendStatus(http.StatusCreated)
+	return utils.SuccessResponse(c, http.StatusCreated, "success created book", nil)
 }
 
-func (h *handler) Update(c *fiber.Ctx) error {
+func (h *Handler) Update(c *fiber.Ctx) error {
 	id, err := strconv.Atoi(c.Params("id"))
 	if err != nil {
-		return c.Status(http.StatusBadRequest).JSON(fiber.Map{"error": "Invalid book ID"})
+		return utils.ErrorResponse(c, http.StatusBadRequest, "Invalid book ID")
 	}
 	var b model.Book
 	if err := c.BodyParser(&b); err != nil {
-		return c.Status(http.StatusBadRequest).JSON(fiber.Map{"error": "Invalid input"})
+		return utils.ErrorResponse(c, http.StatusBadRequest, "Invalid input")
 	}
 	b.ID = id
 	if err := h.svc.Update(b); err != nil {
-		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+		return utils.ErrorResponse(c, http.StatusInternalServerError, err.Error())
 	}
-	return c.SendStatus(http.StatusOK)
+	return utils.SuccessResponse(c, http.StatusOK, "success updated book", b)
 }
 
-func (h *handler) Delete(c *fiber.Ctx) error {
+func (h *Handler) Delete(c *fiber.Ctx) error {
 	id, err := strconv.Atoi(c.Params("id"))
 	if err != nil {
-		return c.Status(http.StatusBadRequest).JSON(fiber.Map{"error": "Invalid book ID"})
+		return utils.ErrorResponse(c, http.StatusBadRequest, "Invalid book ID")
 	}
 	if err := h.svc.Delete(id); err != nil {
-		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+		return utils.ErrorResponse(c, http.StatusInternalServerError, err.Error())
 	}
-	return c.SendStatus(http.StatusNoContent)
+	return utils.SuccessResponse(c, http.StatusNoContent, "success deleted book", nil)
 }
