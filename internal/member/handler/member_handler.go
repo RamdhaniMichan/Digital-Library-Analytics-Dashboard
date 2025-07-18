bdaf25cc50e8671fe009dc0d5ffba3b41d6c3eaa
ramdhanimichan <ramdhani.michan48@gmail.com>
2025-07-19 00:29:47 +0700

    update : reponse handler member

--- internal/member/handler/member_handler.go
+++ internal/member/handler/member_handler.go
@@ -3,6 +3,9 @@ package handler
 import (
 	"digital-library-dashboard/internal/member/model"
 	"digital-library-dashboard/internal/member/service"
+	"digital-library-dashboard/internal/middleware"
+	"digital-library-dashboard/pkg/utils"
+	"net/http"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -13,39 +16,37 @@ type Handler struct {
 
 func RegisterRoutes(r fiber.Router, svc service.Service) {
 	h := Handler{svc: svc}
-
+	r.Use(middleware.RoleMiddleware("admin"))
 	r.Get("/members", h.List)
 	r.Get("/members/:id", h.GetByID)
 	r.Post("/members", h.Create)
-	// r.Put("/members/:id", h.Update)
-	// r.Delete("/members/:id", h.Delete)
 }
 
 func (h *Handler) Create(c *fiber.Ctx) error {
 	var m model.Member
 	if err := c.BodyParser(&m); err != nil {
-		return c.Status(400).JSON(fiber.Map{"error": "invalid input"})
+		return utils.ErrorResponse(c, http.StatusBadGateway, "Invalid input")
 	}
 	err := h.svc.Create(&m)
 	if err != nil {
-		return c.Status(500).JSON(fiber.Map{"error": err.Error()})
+		return utils.ErrorResponse(c, http.StatusInternalServerError, err.Error())
 	}
-	return c.JSON(m)
+	return utils.SuccessResponse(c, http.StatusCreated, "Success created member", m)
 }
 
 func (h *Handler) GetByID(c *fiber.Ctx) error {
 	id, _ := c.ParamsInt("id")
 	member, err := h.svc.GetByID(id)
 	if err != nil {
-		return c.Status(404).JSON(fiber.Map{"error": "member not found"})
+		return utils.ErrorResponse(c, http.StatusNotFound, "Member not found")
 	}
-	return c.JSON(member)
+	return utils.SuccessResponse(c, http.StatusOK, "Success get member", member)
 }
 
 func (h *Handler) List(c *fiber.Ctx) error {
 	members, err := h.svc.List()
 	if err != nil {
-		return c.Status(500).JSON(fiber.Map{"error": err.Error()})
+		return utils.ErrorResponse(c, http.StatusInternalServerError, err.Error())
 	}
-	return c.JSON(members)
+	return utils.SuccessResponse(c, http.StatusOK, "Success get members", members)
 }
