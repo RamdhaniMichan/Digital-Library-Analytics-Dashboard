b5239990a734a5894ff0ab0b0670f2b0e80d7ad6
ramdhanimichan <ramdhani.michan48@gmail.com>
2025-07-19 00:23:29 +0700

    update : connection pool

--- config/config.go
+++ config/config.go
@@ -5,19 +5,32 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	_ "github.com/lib/pq"
 )
 
 func ConnectDB() *sql.DB {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable",
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASS"),
-		os.Getenv("DB_NAME"))
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"))
+
 	db, err := sql.Open("postgres", dsn)
 	if err != nil {
-		log.Fatal("Failed to connect to DB: ", err)
+		log.Fatalf("Failed to open DB: %v", err)
 	}
+
+	if err := db.Ping(); err != nil {
+		log.Fatalf("Failed to ping DB: %v", err)
+	}
+
+	db.SetMaxOpenConns(25)
+	db.SetMaxIdleConns(10)
+	db.SetConnMaxLifetime(5 * time.Minute)
+
+	log.Println("Database connected successfully")
 	return db
 }
